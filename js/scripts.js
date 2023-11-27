2a06c0cab6618fe50643cf44303a691b6f2b4fbc
Arowolo Haleem <arowolohaleem@gmail.com>
2023-11-27 13:46:26 +0100

    add toggle code to html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
-(document).readyState(function() {
-  
-})
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+});
+
+// $(".clickable").click(function() {
+//   $("#walrus-showing").fadeIn();
+//   $("#walrus-hidden").fadeOut();
+// });
+
+// $(".clickable").click(function() {
+//   $("#walrus-showing").fadeToggle();
+//   $("#walrus-hidden").fadeToggle();
+// });
+
+// $(".clickable").click(function() {
+//   $("#walrus-showing").slideDown();
+//   $("#walrus-hidden").slideUp();
+// });
+
+// $(".clickable").click(function() {
+//   $("#walrus-showing").slideToggle();
+//   $("#walrus-hidden").slideToggle();
+// });
